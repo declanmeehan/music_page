6ecf38d38bc4504f3990ecaaf70e8e6e559bc116
Declan <declan.meehan@gmail.com>
2017-11-23 03:03:20 -0600

    making noises for second row of keyboard a to l, good job buddy, keep up the good work

---
+++ main.js
@@ -0,0 +1,6 @@
+//plays audio from pressing certain key
+  window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}" ]`);
+    if(!audio) return; // stop the function from running
+    audio.play();
+  });
